046e84fb1a86313d41d8c15169d43e1421cf1c78
naohiro <k14053kk@aitech.ac.jp>
2017-07-12 00:08:39 +0900

    デバッグログで気圧が取得できるようになった

--- app/src/main/java/com/hobby/soiya/barometricpressurechange/OpenWeatherMapApiInterface.java
+++ app/src/main/java/com/hobby/soiya/barometricpressurechange/OpenWeatherMapApiInterface.java
@@ -1,4 +1,14 @@
 package com.hobby.soiya.barometricpressurechange;
 
+import com.hobby.soiya.barometricpressurechange.data.WeatherContainer;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 public interface OpenWeatherMapApiInterface {
+
+    @GET("/data/2.5/{path}")
+    public Observable<WeatherContainer> get(@Path("path") String path, @Query("zip") String zipcode, @Query("appid") String key);
 }
